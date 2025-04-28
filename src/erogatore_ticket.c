ffe2225e8b7d49b2fbc7dc21362007173a85dd92
Matteo Saracino <saracino.matteo@educ.di.unito.it>
2025-04-28 15:52:49 +0000

    Revert "Added configuration files"
    This reverts commit a589ed409aede239be2019f5c8388abdf3834614

--- src/erogatore_ticket.c
+++ src/erogatore_ticket.c
@@ -15,17 +15,14 @@
 static volatile sig_atomic_t endDay = 0;
 static volatile sig_atomic_t endSimulation = 0;
 
-static void signalHandler(int signo) {
-    switch (signo) {
-        case SIGUSR2:
-            endDay = 1;
-            break;
-        case SIGTERM:
-            endSimulation = 1;
-            break;
-        default:
-            break;
-    }
+// Signal handler per SIGUSR2 -> fine giornata
+static void handle_day_end(int signo) {
+    endDay = 1;
+}
+
+// SIGTERM -> fine simulazione
+static void handle_simulation_end(int signo) {
+    endSimulation = 1;
 }
 
 void SlaveNotifyAndWait(int semID, struct sembuf sops) {
@@ -87,13 +84,13 @@ int main(int argc, char *argv[]) {
 
     // Installa i signal handler
     struct sigaction sa_end = {0};
-    sa_end.sa_handler = signalHandler;
+    sa_end.sa_handler = handle_day_end;
     sigemptyset(&sa_end.sa_mask);
     sa_end.sa_flags = 0;                // senza SA_RESTART
     sigaction(SIGUSR2, &sa_end, NULL);
 
     struct sigaction sa_term = {0};
-    sa_term.sa_handler = signalHandler;
+    sa_term.sa_handler = handle_simulation_end;
     sigemptyset(&sa_term.sa_mask);
     sa_term.sa_flags = 0;
     sigaction(SIGTERM, &sa_term, NULL);
